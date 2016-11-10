bffbeb98d571bd3d7953a6ffae0fdd6a581a7466
burzyk <p.k.burzynski@gmail.com>
2016-11-10 18:30:47 +0000

    Cocoapods

---
+++ AlimakHermes/AlimakHermes/CarClient.swift
@@ -0,0 +1,24 @@
+//
+//  CarClient.swift
+//  AlimakHermes
+//
+//  Created by Pawel Burzynski on 10/11/2016.
+//  Copyright © 2016 JPBNET SOFTWARE. All rights reserved.
+//
+
+import Foundation
+
+class CarClient {
+    
+    public func drive(speed: float_t) {
+        
+    }
+    
+    public func turnLeft() {
+        
+    }
+    
+    public func turnRight() {
+        
+    }
+}
--- AlimakHermes/AlimakHermes/ViewController.swift
+++ AlimakHermes/AlimakHermes/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let resolution = 100.0
+    
+    let threshold = 20.0
+    
+    @IBOutlet var label1: UILabel!
+    
+    @IBOutlet var label2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onDrag(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: self.view)
+        var x = Double(translation.x) / resolution
+        var y = Double(translation.y) / resolution
 
+        x = x > 1 ? 1 : x < -1 ? -1 : x;
+        y = y > 1 ? 1 : y < -1 ? -1 : y;
+        
+        
+        if(sender.state == UIGestureRecognizerState.ended) {
+            x = 0;
+            y = 0;
+        }
+        
+        label1.text = x.description
+        label2.text = y.description
+    }
+    
+    func normalizeDrag(value: Double) {
+        value = max(value - threshold, 0) / resolution
+        value = min(value, 1)
+        
+        
+        x = x > 1 ? 1 : x < -1 ? -1 : x;
+    }
 }
 
